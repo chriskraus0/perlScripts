7c1eedddc1d99dfdaa51a31a23bac77c25a2c51e
christopher <c.kraus@uni-koeln.de>
2016-10-27 16:50:45 +0200

    changed program to avoid isoform errors

--- oinspOut3ToCounts.pl
+++ oinspOut3ToCounts.pl
@@ -21,14 +21,15 @@ my $helpMsg = "HELP\n\n"
 		. "\t\t\tOrtho Inspector output (format 3; tsv table).\n\n"
 		. "--outputTsv\t\tThis option should hold the path and name of the count table.\n\n"
 		. "--geneName\t\tThis option should hold the initial gene-ID or gene-Name to query\n"
-		. "\t\t\tOrtho Inspector.\n\n";
+		. "\t\t\tOrtho Inspector.\n"
+		. "--headerIndex\t\tThis option holds the path to the gene name index file (2 column tsv)\n\n";
 
 ####################
 # USAGE message:
-my $usageMsg = "USAGE: ./oinspOut3ToCounts.pl --oinsp3Tsv=\'<FILE>\' --outputTsv=\'<FILE>\' --geneName=\'<GeneID|GeneName>\'\n\n"
+my $usageMsg = "USAGE: ./oinspOut3ToCounts.pl --oinsp3Tsv=\'<FILE>\' --outputTsv=\'<FILE>\' --geneName=\'<GeneID|GeneName>\' --headerIndex=\'<FILE>\'\n\n"
 		. "$helpMsg";
 
-die "$usageMsg" unless (@ARGV == 3);
+die "$usageMsg" unless (@ARGV == 4);
 
 ####################
 # Get working directory and move to that directory.
@@ -41,16 +42,19 @@ chdir $dir;
 my $oinsp3Tsv;
 my $outputTsv;
 my $geneName;
+my $headerIndex;
 
 GetOptions ("oinsp3Tsv=s" => \$oinsp3Tsv,
 		"outputTsv=s" => \$outputTsv,
-		"geneName=s" => \$geneName)
+		"geneName=s" => \$geneName,
+		"headerIndex=s" => \$headerIndex)
 or die("Error in command line arguments.\n". "$usageMsg");
 
 # Catch arguments not initialized errors:
 die "Error: option \"--oinsp3Tsv\" was not given\n" unless ($oinsp3Tsv);
 die "Error: option \"--queryBatch\" was not given\n" unless ($outputTsv);
 die "Error: option \"--geneName\" was not given\n" unless ($geneName);
+die "Error: option \"--headerIndex\" was not given\n" unless ($headerIndex);
 
 ####################
 # Read Ortho Inspector output tsv table (format 3) and retrieve all orthologs (and paralogs).
@@ -81,6 +85,45 @@ while (<$fh>) {
 		}
 	} 
 }
+
+close $fh;
+
+####################
+# Load header index information.
+
+# Save header index in hash.
+my %orthologGeneRes;
+
+open $fh, "<", $headerIndex or die "Error: $headerIndex: $!\n";
+
+while (<$fh>) {
+	chomp;
+	if (/\A>/) {
+		(my $oinspHeader, my $origHeader) = split /\t/;
+		(my $headerSpecies, my $dummy) = split /\|/, $oinspHeader; 
+
+		$headerSpecies =~ s/\A>//;
+		$oinspHeader =~ s/\A>\w\w\w\|//;
+		$origHeader =~ s/\A>\w\w\w\|//;
+
+		foreach my $species (sort keys %orthologRes) {
+			if ($headerSpecies eq $species) {
+				foreach my $ortholog (sort keys %{ $orthologRes{$species} }) {
+					if ($ortholog eq $oinspHeader) {
+						if ($orthologGeneRes{$species}) {
+							$orthologGeneRes{$species}->{$origHeader} = 1;
+							last;
+						} else {
+							$orthologGeneRes{$species} = { ($origHeader => 1) };
+							last;
+						}
+					}
+				}
+				last;
+			}
+		}
+	}
+}	
 
 close $fh;
 
@@ -122,10 +165,10 @@ $evOrthoRes{'HSA'} = 0;
 
 # Evaluate results.
 
-foreach my $species (sort keys %orthologRes) {
+foreach my $species (sort keys %orthologGeneRes) {
 	my $count = 0;
-	foreach my $ortholog (sort keys %{ $orthologRes{$species} }) {
-		$count += $orthologRes{$species}->{$ortholog};
+	foreach my $ortholog (sort keys %{ $orthologGeneRes{$species} }) {
+		$count += $orthologGeneRes{$species}->{$ortholog};
 	}
 	if ($evOrthoRes{$species}) {
 		$evOrthoRes{$species} += $count;
@@ -141,57 +184,57 @@ open my $ofh, ">", $outputTsv or die "Error: $outputTsv: $!\n";
 
 print $ofh "$geneName\n";
 
-print $ofh "EBR\t",
-		"RCU\t",	
-		"RCT\t",	
-		"TSP\t",	
-		"GOR\t",	
-		"BMA\t",	
-		"LOA\t",	
-		"DIM\t",	
-		"ASU\t",	
-		"BUX\t",	
-		"MHA\t",	
-		"CEL\t",	
+print $ofh "CEL\t",
 		"CBR\t",	
 		"CRE\t",	
 		"CAN\t",	
 		"PPA\t",	
+		"MHA\t",	
+		"BUX\t",	
+		"ASU\t",	
+		"LOA\t",	
+		"DIM\t",	
+		"BMA\t",	
+		"EBR\t",	
+		"TSP\t",	
+		"RCU\t",	
+		"RCT\t",	
+		"GOR\t",	
 		"HDU\t",	
+		"API\t",	
+		"DPU\t",	
+		"TUR\t",	
+		"SMA\t",	
 		"TCA\t",	
 		"DME\t",	
-		"SMA\t",	
-		"TUR\t",	
-		"DPU\t",	
-		"API\t",	
 		"CCA\t",	
 		"SPU\t",	
 		"BFL\t",	
 		"HSA\n";
 
-print $ofh "$evOrthoRes{'EBR'}\t",
-		"$evOrthoRes{'RCU'}\t",	
-		"$evOrthoRes{'RCT'}\t",	
-		"$evOrthoRes{'TSP'}\t",	
-		"$evOrthoRes{'GOR'}\t",	
-		"$evOrthoRes{'BMA'}\t",	
-		"$evOrthoRes{'LOA'}\t",	
-		"$evOrthoRes{'DIM'}\t",	
-		"$evOrthoRes{'ASU'}\t",	
-		"$evOrthoRes{'BUX'}\t",	
-		"$evOrthoRes{'MHA'}\t",	
-		"$evOrthoRes{'CEL'}\t",	
+print $ofh "$evOrthoRes{'CEL'}\t",
 		"$evOrthoRes{'CBR'}\t",	
 		"$evOrthoRes{'CRE'}\t",	
 		"$evOrthoRes{'CAN'}\t",	
 		"$evOrthoRes{'PPA'}\t",	
+		"$evOrthoRes{'MHA'}\t",	
+		"$evOrthoRes{'BUX'}\t",	
+		"$evOrthoRes{'ASU'}\t",	
+		"$evOrthoRes{'LOA'}\t",	
+		"$evOrthoRes{'DIM'}\t",	
+		"$evOrthoRes{'BMA'}\t",	
+		"$evOrthoRes{'EBR'}\t",	
+		"$evOrthoRes{'TSP'}\t",	
+		"$evOrthoRes{'RCU'}\t",	
+		"$evOrthoRes{'RCT'}\t",	
+		"$evOrthoRes{'GOR'}\t",	
 		"$evOrthoRes{'HDU'}\t",	
+		"$evOrthoRes{'API'}\t",	
+		"$evOrthoRes{'DPU'}\t",	
+		"$evOrthoRes{'TUR'}\t",	
+		"$evOrthoRes{'SMA'}\t",	
 		"$evOrthoRes{'TCA'}\t",	
 		"$evOrthoRes{'DME'}\t",	
-		"$evOrthoRes{'SMA'}\t",	
-		"$evOrthoRes{'TUR'}\t",	
-		"$evOrthoRes{'DPU'}\t",	
-		"$evOrthoRes{'API'}\t",	
 		"$evOrthoRes{'CCA'}\t",	
 		"$evOrthoRes{'SPU'}\t",	
 		"$evOrthoRes{'BFL'}\t",	
